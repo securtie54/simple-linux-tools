669ba2e312970f30720f3981c195fe2651a4f5d6
securtie54 <88200933+securtie54@users.noreply.github.com>
2021-09-07 15:48:10 +0300

    Add files via upload

---
+++ project_1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env/python
+
+import os
+os.system("apt-get install nikto")
+os.system("apt-get install figlet")
+os.system("clear")
+os.system("figlet Simple vulnerability Analysis ")
+
+targetip = raw_input("Target Ip ==> ")
+os.system("nikto -h" +targetip)
